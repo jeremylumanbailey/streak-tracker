eb35c5c1ce8739f308f9affdf6a1fd0a14d82212
Jeremy Bailey <jeremylumanbailey@gmail.com>
2022-12-30 02:20:08 -0500

    I think I need to move these tiles into their own component or something

--- components/StreakPage.tsx
+++ components/StreakPage.tsx
@@ -3,8 +3,9 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 import type { RootStackParamList } from '../types'
 import React, { useEffect, useState } from 'react'
 import { deleteSpecificStreak } from '../utils'
-import { secondsSinceStreakStart, timeLeft, timeLeftType } from '../timeUtils'
+import { timeLeft, timeLeftType } from '../timeUtils'
 import { Alert } from 'react-native'
+import { Surface } from 'react-native-paper'
 
 
 type ProfileProps = NativeStackScreenProps<RootStackParamList, 'StreakPage'>
@@ -42,11 +43,52 @@ export default function StreakPage( { route, navigation }: ProfileProps ): JSX.E
 		return () => clearInterval(intervalId) //This is important
 	}, [streakPageData])
  
-	
+	const elevationShadow = 5
   return (
     <View style={styles.container}>
-			<Text>{`You are editing ${streakPageData.streakTitle}.
-			You have had your streak for  ${streakDate.days} days ${streakDate.hours} hours, ${streakDate.minutes} minutes and ${streakDate.seconds} seconds`}</Text>
+			<View style={styles.content}>
+				<View style={styles.verticalCenterItems}>
+					<View style={styles.textContent}>
+						<Text>{`You are editing ${streakPageData.streakTitle}.`}</Text>
+					</View>
+					<View style={styles.allTiles}>
+						<View>
+							<View style={styles.timeContainer}>
+								<Surface style={styles.surface} elevation={elevationShadow}>
+									<Text>{streakDate.seconds}</Text>
+									<Text>seconds</Text>
+								</Surface>
+							</View>
+							<View style={styles.timeContainer}>
+								<Surface style={styles.surface} elevation={elevationShadow}>
+									<Text>{streakDate.hours}</Text>
+									<Text>hours</Text>
+								</Surface>
+							</View>
+						</View>
+
+						<View> 
+							<View style={styles.timeContainer}>
+								<Surface style={styles.surface} elevation={elevationShadow}>
+									<Text>{streakDate.minutes}</Text>
+									<Text>minutes</Text>
+								</Surface>
+							</View>
+
+							<View style={styles.timeContainer}>
+								<Surface style={styles.surface} elevation={elevationShadow}>
+									<Text>{streakDate.days}</Text>
+									<Text>days</Text>
+								</Surface>
+							</View>
+						</View>
+						
+					</View>
+
+				</View>
+				
+
+			</View>
 			<View style={styles.actionButtons}>
 					<Button title="Save/Back to home" onPress={(): void => navigation.navigate('Home')} />
 					<Button title="Delete" onPress={handlePress} />
@@ -56,16 +98,47 @@ export default function StreakPage( { route, navigation }: ProfileProps ): JSX.E
 }
 
 const styles = StyleSheet.create({
+	container: {
+		margin: 0,
+    height: '100%',
+		width: '100%',
+    backgroundColor: 'red',
+  },
+	content: {
+		backgroundColor: 'green',
+		flex:1,
+		alignItems: 'center',
+		flexDirection: 'row',
+	},
+	verticalCenterItems: {
+
+	},
 	actionButtons: {
+		flex: 1,
 		flexDirection: 'row',
 		justifyContent:"space-evenly",
 		alignItems: 'center',
-		width: '100%'		
+		backgroundColor: 'blue'
 	},
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+	allTiles:{
+		width: '100%',
+		flex: 1,
+		flexDirection: 'row',
+	},
+	timeContainer:{
+		flex: 1,
+		flexDirection: 'column',
+		//height: '25%'
+	},
+	surface: {
+		flex: 1,
+		textAlign: 'center',
+		textAlignVertical: 'center',
+		alignItems: 'center',
     justifyContent: 'center',
-  },
+	},
+	textContent: {
+		flex: 1,
+    justifyContent: 'center',
+	}
 })
